455ad46bc40317d82cd0e471a9eff7cfe87f228c
tcpolymath <40719495+tcpolymath@users.noreply.github.com>
2019-01-13 15:56:41 +0000

    Create transfer_command_stream.py

---
+++ transfer_command_stream.py
@@ -0,0 +1,30 @@
+from beem import Steem
+from beem.blockchain import Blockchain
+from beem.instance import set_shared_steem_instance
+from beem.nodelist import NodeList
+
+
+nodes = NodeList()
+nodes.update_nodes()
+steem = Steem(node=nodes.get_nodes())
+set_shared_steem_instance(steem)
+
+
+def transfer_command_stream(account):
+        blockchain=Blockchain(mode="head")
+        stream=blockchain.stream(opNames=['transfer'], raw_ops=False)
+        for transfer in stream:
+                if transfer['to'] == account:
+                        amount = float(transfer['amount']['amount'])/1000
+                        if transfer['amount']['nai'] == "@@000000021":
+                                currency = "STEEM"
+                        elif transfer['amount']['nai'] == "@@000000013":
+                                currency = "SBD"
+                        else:
+                                currency = ""
+                        output_dict = {"from": transfer['from'], "amount": amount, "currency": currency, "memo": transfer['memo'].split()}
+                        yield output_dict
+
+commands = transfer_command_stream("tcpolymath")
+for command in commands:
+        print command
